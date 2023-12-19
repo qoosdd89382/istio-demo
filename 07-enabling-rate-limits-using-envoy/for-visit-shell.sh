0c734a1fc0c1e56be1d0e35806952eb0f71838a0
Danny CHUN JEN Yang <yjring@gmail.com>
2023-12-19 16:38:34 +0800

    change shell instructions for 07 lab

--- 07-enabling-rate-limits-using-envoy/for-visit-shell.sh
+++ 07-enabling-rate-limits-using-envoy/for-visit-shell.sh
@@ -2,5 +2,6 @@
 
 for (( i=1; i<=15; i++ ))
 do
+  echo "Attempt $i"
   kubectl exec "$(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}')" -c ratings -- curl -s productpage:9080/productpage -o /dev/null -w "%{http_code}\n"
 done
